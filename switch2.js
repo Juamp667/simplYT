69525cb63aa06a823797826adc8cbfc74e338e35
Juamp667 <jmpadillagalera@gmail.com>
2025-10-17 08:17:44 +0200

    First commit

---
+++ switch2.js
@@ -0,0 +1,43 @@
+
+
+
+switch2 = document.getElementById("switchRightFeed")
+switch2.addEventListener('change',()=>{
+    chrome.tabs.query({ url: "https://www.youtube.com/watch*" , }, (tabs) => {
+      const isChecked = switch2.checked;
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: (checked) => {
+          checked ? disableWatch() : enableWatch()
+        },
+        args : [isChecked]
+      });
+    });
+})
+
+function disableWatch(){
+  secondary = document.getElementById("secondary")
+
+  const v = [secondary]
+  for (let i=0; i<v.length;i++){
+      v[i].style.visibility = "hidden"
+      v[i].style.position = "absolute"
+  }
+
+  primary = document.getElementById("primary")
+  primary.style.minWidth = "80vw" 
+
+}
+
+function enableWatch(){
+
+  const v = [secondary]
+  for (let i=0; i<v.length;i++){
+      v[i].style.visibility = "visible"
+      v[i].style.position = "static"
+  }
+
+  primary.style.minWidth = "0" 
+
+}
+
